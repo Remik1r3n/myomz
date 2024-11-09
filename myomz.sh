feba0ea5db3a8c3be8d1b06e255ba68952a5b450
Remik1r3n <62464927+Remik1r3n@users.noreply.github.com>
2024-11-09 17:56:51 +0800

    Single user install support by GPT

--- myomz.sh
+++ myomz.sh
@@ -4,129 +4,141 @@
 # by Remik1r3n
 
 command_exists() {
-        command -v "$@" >/dev/null 2>&1
+    command -v "$@" >/dev/null 2>&1
 }
+
+is_macos() {
+    [ "$(uname)" = "Darwin" ]
+}
+
+parse_arguments() {
+    SINGLE_USER_INSTALL=false
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            --single-user-install)
+                SINGLE_USER_INSTALL=true
+                ;;
+            *)
+                echo "Unknown argument: $1"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+}
+
 self_check() {
-if ! command_exists zsh; then
-    echo "FATAL: zsh is not installed. Please install it first."
-    exit 1
-fi
-
-if ! command_exists wget; then
-    if ! command_exists curl; then
-        echo "FATAL: wget or curl is not installed. Please install one or both of it(wget recommended)."
+    if ! command_exists zsh; then
+        echo "FATAL: zsh is not installed. Please install it first."
         exit 1
-    else
-        echo "WARNING: wget is not installed, falling back to curl. this may cause error!"
-        echo "It is strongly recommended that you press Ctrl+C now, install wget, then run this script again."
-        echo "Waiting for 10s.."
-        sleep 10s
-        USED_DOWNLOADER='curl'
     fi
-else
-    USED_DOWNLOADER='wget'
-fi
 
-if ! command_exists git; then
-    echo "FATAL: git is not installed. Please install it first."
-    exit 1
-fi
+    if ! command_exists wget && ! command_exists curl; then
+        echo "FATAL: wget or curl is not installed. Please install one or both."
+        exit 1
+    fi
 
-if [ -d "/usr/share/oh-my-zsh" ]; then
-    echo "FATAL: /usr/share/oh-my-zsh exists! May be already installed? \nIf not, please delete it before install. Otherwise, check your configuration."
-    exit 1
-fi
+    if ! command_exists git; then
+        echo "FATAL: git is not installed. Please install it first."
+        exit 1
+    fi
 
-if [ `whoami` != "root" ];then
-	echo "FATAL: Use root user."
-	exit 1
-fi
+    if [ "$SINGLE_USER_INSTALL" = false ] && ! is_macos && [ "$(id -u)" -ne 0 ]; then
+        echo "FATAL: Please run as root."
+        exit 1
+    fi
 }
-self_check
 
-echo "MyOMZ Script - by Remi 2021-2024"
-echo ""
-echo "Pick a mirror:"
-echo "[G] - GitHub -- Best compatibility, but may be slow in China Mainland."
-echo "[C] - China  -- Use proxy service to accelerate GitHub access in China Mainland."
-read -p "Select > " MIRRORANSWER
+download_script() {
+    local url="$1"
+    local dest="$2"
 
-echo "Now downloading install script.."
+    if [ "$USED_DOWNLOADER" = "curl" ]; then
+        curl -Lo "$dest" "$url"
+    elif [ "$USED_DOWNLOADER" = "wget" ]; then
+        wget -O "$dest" "$url"
+    fi
+}
 
-rm /tmp/omz_install.sh
+install_plugin() {
+    local repo_url="$1"
+    local dest_dir="$2"
 
-if [ $USED_DOWNLOADER = curl ]; then
-    DOWNLOAD_CMD="$USED_DOWNLOADER -Lo /tmp/omz_install.sh" 
-elif [ $USED_DOWNLOADER = wget ]; then
-    DOWNLOAD_CMD="$USED_DOWNLOADER -O /tmp/omz_install.sh"
-fi
+    git clone "$repo_url" "$dest_dir"
+}
 
-if [ "$MIRRORANSWER" = "G" -o "$MIRRORANSWER" = "g" ]; then
-    $DOWNLOAD_CMD https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-elif [ "$MIRRORANSWER" = "C" -o "$MIRRORANSWER" = "c" ]; then
-    $DOWNLOAD_CMD https://gh-proxy.com/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-elif [ "$MIRRORANSWER" = "e" -o "$MIRRORANSWER" = "E" ]; then
-    $DOWNLOAD_CMD https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
+main() {
+    parse_arguments "$@"
+    self_check
 
-else
-    echo "FATAL: Selection invaild. "
-    exit 1
-fi
+    if [ "$SINGLE_USER_INSTALL" = true ] || is_macos; then
+        INSTALL_PATH="$HOME/.oh-my-zsh"
+    else
+        INSTALL_PATH="/usr/share/oh-my-zsh"
+    fi
 
-chmod +x /tmp/omz_install.sh
+    echo "MyOMZ Script - by Remi 2021-2024"
+    echo ""
+    echo "Pick a mirror:"
+    echo "[G] - GitHub -- Best compatibility, but may be slow in China Mainland."
+    echo "[C] - China  -- Use proxy service to accelerate GitHub access in China Mainland."
+    echo "[E] - Gitee -- Alternative for China Mainland."
+    read -p "Select > " MIRRORANSWER
 
-echo "----- RUNNING OH-MY-ZSH INSTALL SCRIPT -----"
-if [ "$MIRRORANSWER" = "G" -o "$MIRRORANSWER" = "g" ]; then
-    RUNZSH=no ZSH=${ZSH:-/usr/share/oh-my-zsh} /tmp/omz_install.sh
-elif [ "$MIRRORANSWER" = "C" -o "$MIRRORANSWER" = "c" ]; then
-    RUNZSH=no ZSH=${ZSH:-/usr/share/oh-my-zsh} REPO=${REPO:-ohmyzsh/ohmyzsh} REMOTE=${REMOTE:-https://gh-proxy.com/https://github.com/${REPO}.git} /tmp/omz_install.sh
-elif [ "$MIRRORANSWER" = "e" -o "$MIRRORANSWER" = "E" ]; then
-    RUNZSH=no ZSH=${ZSH:-/usr/share/oh-my-zsh} REPO=${REPO:-mirrors/oh-my-zsh} REMOTE=${REMOTE:-https://gitee.com/${REPO}.git} /tmp/omz_install.sh
-fi
-if [ $? != 0 ];then
-    echo "ERROR: Install script returned error! INSTALLATION FAILED!!"
-    read -p "Ignore this error? (DO NOT!) (y/N)" ANSWER
-    if [ "$ANSWER" != "Y" -o "$ANSWER" != "y" ]; then
+    echo "Now downloading install script.."
+
+    rm -f /tmp/omz_install.sh
+
+    case "$MIRRORANSWER" in
+        G|g)
+            download_script "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" "/tmp/omz_install.sh"
+            ;;
+        C|c)
+            download_script "https://gh-proxy.com/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" "/tmp/omz_install.sh"
+            ;;
+        E|e)
+            download_script "https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh" "/tmp/omz_install.sh"
+            ;;
+        *)
+            echo "FATAL: Invalid selection."
+            exit 1
+            ;;
+    esac
+
+    chmod +x /tmp/omz_install.sh
+
+    echo "----- RUNNING OH-MY-ZSH INSTALL SCRIPT -----"
+    RUNZSH=no ZSH="$INSTALL_PATH" /tmp/omz_install.sh
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Install script returned an error! INSTALLATION FAILED!!"
         exit 1
     fi
-fi
-echo "----- OH-MY-ZSH INSTALL SCRIPT COMPLETED -----"
-cp /usr/share/oh-my-zsh/templates/zshrc.zsh-template /usr/share/oh-my-zsh/templates/zshrc.zsh-template.original.bak
-rm ~/.zshrc
 
-echo "Now patching zshrc file.."
+    echo "----- OH-MY-ZSH INSTALL SCRIPT COMPLETED -----"
+    cp "$INSTALL_PATH/templates/zshrc.zsh-template" "$INSTALL_PATH/templates/zshrc.zsh-template.original.bak"
+    rm -f ~/.zshrc
 
-sed -i "s#\$HOME/.oh-my-zsh#\"/usr/share/oh-my-zsh\"#g"  /usr/share/oh-my-zsh/templates/zshrc.zsh-template
-sed -i "s#robbyrussell#gentoo#g"  /usr/share/oh-my-zsh/templates/zshrc.zsh-template
-sed -i "s#plugins=(git)#plugins=(git extract sudo zsh-syntax-highlighting zsh-autosuggestions)#g"  /usr/share/oh-my-zsh/templates/zshrc.zsh-template
+    echo "Now patching zshrc file.."
+    sed -i "s#\$HOME/.oh-my-zsh#$INSTALL_PATH#g" "$INSTALL_PATH/templates/zshrc.zsh-template"
+    sed -i "s#robbyrussell#gentoo#g" "$INSTALL_PATH/templates/zshrc.zsh-template"
+    sed -i "s#plugins=(git)#plugins=(git extract sudo zsh-syntax-highlighting zsh-autosuggestions)#g" "$INSTALL_PATH/templates/zshrc.zsh-template"
 
-echo "Now installing zsh-syntax-highlighting."
+    echo "Now installing zsh-syntax-highlighting."
+    install_plugin "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$INSTALL_PATH/plugins/zsh-syntax-highlighting"
 
-if [ "$MIRRORANSWER" = "G" -o "$MIRRORANSWER" = "g" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/plugins/zsh-syntax-highlighting
-elif [ "$MIRRORANSWER" = "C" -o "$MIRRORANSWER" = "c" ]; then
-    git clone https://gh-proxy.com/https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/plugins/zsh-syntax-highlighting
-elif [ "$MIRRORANSWER" = "e" -o "$MIRRORANSWER" = "E" ]; then
-    git clone https://gitee.com/mirror-github/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/plugins/zsh-syntax-highlighting
-fi
+    echo "Now installing zsh-autosuggestions."
+    install_plugin "https://github.com/zsh-users/zsh-autosuggestions.git" "$INSTALL_PATH/plugins/zsh-autosuggestions"
 
-echo "Now installing zsh-autosuggestions."
+    echo "Applying patched zshrc file.."
+    cp "$INSTALL_PATH/templates/zshrc.zsh-template" ~/.zshrc
 
-if [ "$MIRRORANSWER" = "G" -o "$MIRRORANSWER" = "g" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/oh-my-zsh/plugins/zsh-autosuggestions
-elif [ "$MIRRORANSWER" = "C" -o "$MIRRORANSWER" = "c" ]; then
-    git clone https://gh-proxy.com/https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/oh-my-zsh/plugins/zsh-autosuggestions
-elif [ "$MIRRORANSWER" = "e" -o "$MIRRORANSWER" = "E" ]; then
-    git clone https://gitee.com/mirror-github/zsh-autosuggestions.git /usr/share/oh-my-zsh/plugins/zsh-autosuggestions
-fi
+    echo ""
+    echo "------------------"
+    echo "All done! Run zsh to try it out."
+    echo "If you want to let oh-my-zsh work for another user, switch to that user and execute:"
+    echo "cp $INSTALL_PATH/templates/zshrc.zsh-template ~/.zshrc"
 
-echo "Applying patched zshrc file.."
-cp /usr/share/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+    exit 0
+}
 
-echo ""
-echo "------------------"
-echo "All done! Run zsh to try it out."
-echo "if you want to let oh-my-zsh works in other user, switch to that user and execute:"
-echo "cp /usr/share/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc"
-
-exit 0
+main "$@"
